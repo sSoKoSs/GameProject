3bd2ff90142087d2ed228c6b5eb46c2e48b0390c
Kostas <komi1997@gmail.com>
2017-11-23 17:45:32 +0200

    Made the Autologin After you login/register once you can save your users id to the config.txt and when you get back in you can "autologin" (it just uses the saved Userid to start the game)

--- GameProject/RunGame.py
+++ GameProject/RunGame.py
@@ -8,9 +8,23 @@ def main():
     #TODO check for config to auto load Name
 
     print "Welcome to this roguelike (the game is not fun its just to test out the sql database)"
-    awnser = raw_input("(L)ogin or (R)egister > ")
-    if awnser == "L" or awnser == "l":
+    awnser = raw_input("(L)ogin or (R)egister or (A)utoLogin > ")
+
+    if awnser == "A" or awnser == "a":
+        try:
+            conf = open("config.txt","r")
+            UserID = conf.read()
+            conf.close()
+            startGame(UserID)
+            return
+        except:
+            awnser = "NLY"
+
+    if awnser == "L" or awnser == "l" or awnser == "NLY":
         os.system("cls")
+        if awnser == "NLY":
+            print "You haven't logged/registered in yet."
+
         while True:
             print "Login here."
             name = raw_input("Name > ")
@@ -20,8 +34,9 @@ def main():
             result, userID = sqlcon.searchNamesForID(name)
             if result:
                 if password == sqlcon.getPasswordForID(userID):
+                    autoConnect(userID)
                     startGame(userID)
-                    break
+                    return
             os.system("cls")
             print "Wrong credentials. Try again"
 
@@ -46,8 +61,17 @@ def main():
                 print "wrong password try again"
                 print "Register below."
                 continue
+
             sqlcon.newUser(name, password)
             main() # to login again (just to make sure)
+
+def autoConnect(UserID):
+    print "Do you want to auto-join the next time? (y/n)"
+    auto = raw_input("Auto > ")
+    if auto == "y" or auto == "Y":
+        with open("config.txt","w") as conf:
+            conf.write(str(UserID))
+        print "created conf.txt"
 
 def startGame(userID):
     sqlcon.endConnection()
